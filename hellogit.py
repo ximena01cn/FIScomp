f53239e9de366ed51df30515eb8b818bc53c193c
ximena cruz <cruznavaximena@gmail.com>
2024-01-22 19:09:15 -0600

    nice

--- hellogit.py
+++ hellogit.py
@@ -4,3 +4,11 @@ print('hola')
 ## versión guardada
 
 ## tamién tenemos un git reset
+
+## para volver a guardar soolo ponemos git add
+
+## para guardar comandos podemos usar alias, solo hay que poner
+## git config alias.nombre del alias 'comando que queremos guardar'
+
+## si yo se que nunca voy a guardar un fichero puedo usar git ignore para que me deje de aparcer en rojo
+## con git diff podemos ver que cambios hicimos respecto a la ultima modificaión
