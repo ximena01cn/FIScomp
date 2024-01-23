b41b083550d547a718f33a2440bbe2d65af656ba
ximena cruz <cruznavaximena@gmail.com>
2024-01-22 18:44:29 -0600

    Ponemos mas comentarios

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,6 @@
 ## ya tenemos un fichero de codigo 
 print('hola')
+## con git checkout no me guarda los cambios realizados en los ficheros y se va a la ultima 
+## versión guardada
+
+## tamién tenemos un git reset
